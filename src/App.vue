d555d60d3c26ca79ac2b77a190e1997156f25432
Sphirye <68203934+Sphirye@users.noreply.github.com>
2021-11-21 02:22:24 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <HeaderComponent/>
+    <DrawerComponent/>
+    <v-main>
+      <router-view/>
+      <FooterComponent/>
+    </v-main>
+    <DialogComponent/>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator"
+import HeaderComponent from "@/components/HeaderComponent.vue"
+import DrawerComponent from "@/components/DrawerComponent.vue"
+import FooterComponent from "@/components/FooterComponent.vue"
+import DialogComponent from "@/components/DialogComponent.vue"
+
+@Component({
+  components: { HeaderComponent, DrawerComponent, FooterComponent, DialogComponent }
+})
+
+export default class App extends Vue {
+
+}
+
+</script>
